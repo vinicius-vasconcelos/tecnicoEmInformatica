e16de435636da0e9ef6a82e4cfb510f4f6b91119
vinicius-vasconcelos <viniciussouzav@gmail.com>
2019-09-17 22:03:28 -0300

    arrumando agenda 3.0 email

--- UC_5/agenda_n/3.0/fale.php
+++ UC_5/agenda_n/3.0/fale.php
@@ -1,6 +1,9 @@
 <?php
     if(isset($_POST['btGo'])) {
         //$dia = date('d/m/Y');
+
+
+
         $mensagemResp = '
                         <div class="msg-fale">
                             <h2>Obrigado</h2>
@@ -9,18 +12,18 @@
                         </div>
                     ';
         
-        if(PATH_SEPARATOR == ';')
+        /*if(PATH_SEPARATOR == ';')
             $quebraLinha = "\r\n";
         else
-            $quebraLinha = "\n";
+            $quebraLinha = "\n";*/
 
         $nome = utf8_decode(@$_POST['txtNome']);
         $emailVisitante = utf8_decode(@$_POST['txtEmail']);
         $assunto = utf8_decode(@$_POST['textAssunto']);
         $mensagem = utf8_decode(@$_POST['txtMensagem']);
-        $para = "vinicius.testes@outlook.com";
+        $para = "viniciussouzav@gmail.com"; 
 
-        $email = "vinicius.testes@outlook.com";
+        /*$email = "viniciussouzav@gmail.com";
         //$ip = getenv('REMOTE_ADDR');
 
         $mensagemHTML = 'Nome' . $nome . '<br> E-mail' . 
@@ -30,11 +33,27 @@
             $headers = "MIME-Version 1.1" . $quebraLinha . "<br>";
             $headers .= "Content-type: text/html; charset=iso-8859-1" . $quebraLinha;
             $headers .= "From" . $email . $quebraLinha;
+            $headers .= "Cc: " . $emailvisitante . $quebra_linha;
+            $headers .= "Reply-To: " . $emailvisitante . $quebra_linha;
+            //Note que o email do remetente será usado no campo Reply-To (responder para)
+            if ( !mail( $para, $assunto, $mensagemHTML, $headers, "-r" . $email ) ) {		//se for postfix
+                $headers .= "Return-Path: " .$para . $quebra_linha;
+                //se "não for postfix"
+                mail( $para, $assunto, $mensagemHTML, $headers );
+            }*/
+
+            $subject = "Email recebido pelo site: " . $nome;
+            $texto2 = "Nome: " .nome . "\n";
+            $texto2 .= "Email: " .emailVisitante . "\n";
+            $texto2 .= "Mensagem: " .mensagem . "\n";
+            mail($para, $subject, $text2, "From: " .$emailVisitante);
+
+            print_r($mensagem);
 
     }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
